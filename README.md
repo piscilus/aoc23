b98d4f602ae7f1f1b91a03660929e1dbaac3d90e
ppisciluss <piscilus@kxse.de>
2023-12-01 18:25:35 +0100

    Initial commit of project

---
+++ README.md
@@ -0,0 +1,55 @@
+# Advent of Code 2023
+
+![Last Commit on GitHub](https://img.shields.io/github/license/piscilus/aoc23)
+![Last Commit on GitHub](https://img.shields.io/github/last-commit/piscilus/aoc23)
+
+This is my participation in the [Advent of Code](https://adventofcode.com/2023)
+event for the year 2023.
+
+Enjoy the Advent season!
+
+🌟🎄🎅🎁🔔🕯️
+
+## Status
+
+| day | part 1 | part 2 |
+|-----|--------|--------|
+| 01  |        |        |
+| 02  |        |        |
+| 03  |        |        |
+| 04  |        |        |
+| 05  |        |        |
+| 06  |        |        |
+| 07  |        |        |
+| 08  |        |        |
+| 09  |        |        |
+| 10  |        |        |
+| 11  |        |        |
+| 12  |        |        |
+| 13  |        |        |
+| 14  |        |        |
+| 15  |        |        |
+| 16  |        |        |
+| 17  |        |        |
+| 18  |        |        |
+| 19  |        |        |
+| 20  |        |        |
+| 21  |        |        |
+| 22  |        |        |
+| 23  |        |        |
+| 24  |        |        |
+| 25  |        |        |
+
+## Implementation details
+
+The code is written in C without any use of libraries except standard library.
+Every day is a separate program. The puzzle input is stored two files:
+
+- data_example.txt (+ variants)
+- data_contest.txt (not under version control)
+
+```console
+$> ./program data_example.txt
+```
+
+I use gcc, GNU make, Visual Studio Code for development and debugging.
---
+++ src/global.mk
@@ -0,0 +1,67 @@
+.PHONY: all debug profile clean
+.DEFAULT_GOAL = debug
+
+OBJECTDIR = ./objects
+OBJECTDIR_PROFILE = ./$(OBJECTDIR)/profile
+OBJECTDIR_DEBUG = ./$(OBJECTDIR)/debug
+
+TARGET_EXT =
+
+CC = gcc-13
+CFLAGS =\
+	-c\
+	-std=c2x\
+	-Wall\
+	-Wextra\
+	-Wpedantic\
+	-fsanitize=undefined\
+	-fsanitize-undefined-trap-on-error
+CFLAGS_PROFILE =\
+	-DNDEBUG\
+	-pg\
+	-O1
+CFLAGS_DEBUG =\
+	-g\
+	-O0
+
+LD = gcc
+LDFLAGS =\
+	-fsanitize=undefined\
+	-fsanitize-undefined-trap-on-error
+LDFLAGS_PROFILE =\
+	-pg
+LDFLAGS_DEBUG =\
+	-Wl,-Map -Wl,debug/$(TARGET_NAME).map
+
+OBJECTS_PROFILE = $(addprefix $(OBJECTDIR_PROFILE)/, $(SOURCES:.c=.o) )
+OBJECTS_DEBUG = $(addprefix $(OBJECTDIR_DEBUG)/, $(SOURCES:.c=.o) )
+DEPENDS_PROFILE = $(addprefix $(OBJECTDIR_PROFILE)/, $(SOURCES:.c=.d) )
+DEPENDS_DEBUG = $(addprefix $(OBJECTDIR_DEBUG)/, $(SOURCES:.c=.d) )
+
+-include $(DEPENDS_PROFILE)
+-include $(DEPENDS_DEBUG)
+
+$(OBJECTDIR_PROFILE)/%.o: %.c
+	-mkdir -p $(@D)
+	$(CC) $(INCLUDES) $(CFLAGS) $(CFLAGS_PROFILE) $(DEFINES) -MMD -MP -o $@ -l $(@D) $<
+
+$(OBJECTDIR_DEBUG)/%.o: %.c
+	-mkdir -p $(@D)
+	$(CC) $(INCLUDES) $(CFLAGS) $(CFLAGS_DEBUG) $(DEFINES) -MMD -MP -o $@ -l $(@D) $<
+
+debug/$(TARGET_NAME)$(TARGET_EXT): $(OBJECTS_DEBUG)
+	mkdir -p debug
+	$(LD) $(OBJECTS_DEBUG) $(LDFLAGS) $(LDFLAGS_DEBUG) -o $@
+
+profile/$(TARGET_NAME)$(TARGET_EXT): $(OBJECTS_PROFILE)
+	mkdir -p profile
+	$(LD) $(OBJECTS_PROFILE) $(LDFLAGS) $(LDFLAGS_PROFILE) -o $@
+
+debug: debug/$(TARGET_NAME)$(TARGET_EXT)
+profile: profile/$(TARGET_NAME)$(TARGET_EXT)
+all: clean debug profile
+
+clean:
+	rm -rfd $(OBJECTDIR)
+	rm -rfd debug
+	rm -rfd profile
